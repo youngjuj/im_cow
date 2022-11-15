96d07cae4b7bbd1a7f6f11c6d711ff10ae14468a
yougju <alti100@likelion.org>
2022-11-15 19:54:59 +0900

    fixed

--- app/src/main/java/com/example/wintopia/view/list/ListFragment.kt
+++ app/src/main/java/com/example/wintopia/view/list/ListFragment.kt
@@ -7,30 +7,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wintopia.R
+import com.example.wintopia.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
     
 //    lateinit var adapter: ListVOAdapter
-    private var mBinding: FragmentListBinding? = null
-    private val binding get() = mBinding!!
+//    private var mBinding: FragmentListBinding? = null
+//    private val binding get() = mBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        mBinding = FragmentListBinding.inflate(inflater, container, false)
+//        mBinding = FragmentListBinding.inflate(inflater, container, false)
 
         var data = arrayListOf<ListVO>()
         data.add(ListVO("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrcg48Fej-S3muJwRGLbtfNcWcHwEKKfcbrA&usqp=CAU",
         "분홍얼룩이", "22111001"))
 
-        binding.rvList.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvList.adapter = ListVOAdapter(data)
+//        binding.rvList.layoutManager = LinearLayoutManager(requireContext())
+//        binding.rvList.adapter = ListVOAdapter(data)
 
         
-        return binding.root
+        return container
     }
 
 }
