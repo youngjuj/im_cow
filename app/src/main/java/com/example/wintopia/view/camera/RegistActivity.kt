9aea056ac9779eae502c6f080009a659451a74d0
yougju <alti100@likelion.org>
2022-11-16 17:19:17 +0900

    bottom navi fixed

--- app/src/main/java/com/example/wintopia/view/camera/RegistActivity.kt
+++ app/src/main/java/com/example/wintopia/view/camera/RegistActivity.kt
@@ -2,10 +2,33 @@ package com.example.wintopia.view.camera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.wintopia.R
+import com.example.wintopia.databinding.ActivityRegistBinding
 
 class RegistActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityRegistBinding
+    val viewModel: RegistViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_regist)
+//        setContentView(R.layout.activity_regist)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_regist)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+
+        observeData()
+
+        binding.btnRegistCancel.setOnClickListener {  }
+
+        binding.btnRegistRegist.setOnClickListener {  }
+
+
+
+    }
+    fun observeData() {
+
     }
 }
