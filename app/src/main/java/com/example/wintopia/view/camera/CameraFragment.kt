05aaa4046f704ee24eec8c479eda421610196ecf
yougju <alti100@likelion.org>
2022-11-21 13:05:49 +0900

    camera binding successed

--- app/src/main/java/com/example/wintopia/view/camera/CameraFragment.kt
+++ app/src/main/java/com/example/wintopia/view/camera/CameraFragment.kt
@@ -6,8 +6,11 @@ import android.content.ContentResolver
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -19,6 +22,7 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
@@ -27,11 +31,19 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.wintopia.R
 import com.example.wintopia.databinding.FragmentCameraBinding
 import com.example.wintopia.view.main.MainViewModel
+import java.io.File
+import java.io.IOException
 import java.lang.Exception
+import java.sql.Date
+import java.text.SimpleDateFormat
 import java.util.jar.Manifest
 
 class CameraFragment : Fragment() {
     private var show:Boolean? = null
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val GALLERY = 2
+    lateinit var currentPhotoPath: String
+
 
     lateinit var binding: FragmentCameraBinding
 
@@ -42,16 +54,14 @@ class CameraFragment : Fragment() {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
 
+        // data binding
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_camera, container, false)
+
+        // data for floatting buttons
         arguments?.let {
             show = it.getBoolean("data") }
 
-        binding.fbCameraCam.setOnClickListener{
-            Toast.makeText(requireActivity(), "fbCameraCam", Toast.LENGTH_SHORT).show()
-            if(checkPermission()) dispatchTakePictureIntent() else requestPermission()
-        }
-        Log.v("CF show", show.toString())
-
+        // BottomNavi multipleClick options
         if (show!!) {
             binding.fbCameraCam.visibility = View.VISIBLE
             binding.fbCameraGal.visibility = View.VISIBLE
@@ -60,21 +70,26 @@ class CameraFragment : Fragment() {
             binding.fbCameraGal.visibility = View.GONE
         }
 
-
-
+        // camera floatting button onClickListener
+        binding.fbCameraCam.setOnClickListener{
+            Toast.makeText(requireActivity(), "fbCameraCam", Toast.LENGTH_SHORT).show()
+            if(checkPermission()) dispatchTakePictureIntent() else requestPermission()
+        }
 
 
             // Inflate the layout for this fragment
             return binding.root
         }
 
+    // camera 접근 권한 요청
     private fun requestPermission() {
         ActivityCompat.requestPermissions(
             requireActivity(),
             arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, CAMERA),
-            1)
+            REQUEST_IMAGE_CAPTURE)
     }
 
+    // camera 접근 권한 체크
     private fun checkPermission(): Boolean {
 
         return (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
@@ -83,6 +98,7 @@ class CameraFragment : Fragment() {
                 == PackageManager.PERMISSION_GRANTED)
     }
 
+    // 권한 요청 결과
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -97,35 +113,91 @@ class CameraFragment : Fragment() {
         }
     }
 
-    private val REQUEST_IMAGE_CAPTURE = 2
+//    private fun dispatchTakePictureIntent() {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+//            takePictureIntent.resolveActivity(requireActivity().packageManager).also {
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+//            }
+//        }
+//    }
+
     private fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-            }
-        }
+                Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                    if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+                        val photoFile: File? =
+                            try {
+                                createImageFile()
+                            } catch (ex: IOException) {
+                                Log.d("Camera오류", "그림파일 만드는 중 에러생김")
+                                null
+                            }
+                        if (Build.VERSION.SDK_INT < 24) {
+                            if(photoFile != null) {
+                                val photoURI = Uri.fromFile(photoFile)
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                            }
+                        } else {
+                            photoFile?.also {
+                                val photoURI: Uri = FileProvider.getUriForFile(
+                                    requireContext(), "com.example.wintopia.fileprovider", it
+                                )
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                            }
+                        }
+                    }
+
+    }
     }
 
-    private val GALLERY = 3
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(java.util.Date())
+        val storageDir: File? = requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply { currentPhotoPath = absolutePath }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode == Activity.RESULT_OK) {
-            if (requestCode == GALLERY) {
-                var imageData: Uri? = data?.data
-                Toast.makeText(requireContext(), imageData.toString(), Toast.LENGTH_SHORT).show()
-                try {
-                    val bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, imageData)
-                    binding.imgCattle.setImageBitmap(bitmap)
-                } catch (e: Exception) {
-                    e.printStackTrace()
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE -> {
+                if(resultCode == Activity.RESULT_OK) {
+                    val file = File(currentPhotoPath)
+                    if (Build.VERSION.SDK_INT < 28) {
+                        val bitmap = MediaStore.Images.Media
+                            .getBitmap(requireActivity().contentResolver, Uri.fromFile(file))
+                        binding.imgCameraPic.setImageBitmap(bitmap)
+                    } else {
+                        val decode = ImageDecoder.createSource(requireActivity().contentResolver,
+                        Uri.fromFile(file))
+                        val bitmap = ImageDecoder.decodeBitmap(decode)
+                        binding.imgCameraPic.setImageBitmap(bitmap)
+                    }
                 }
             }
-            else if (requestCode == REQUEST_IMAGE_CAPTURE) {
-                val imageBitmap: Bitmap? = data?.extras?.get("data") as Bitmap
-                binding.imgCattle.setImageBitmap(imageBitmap)
-            }
         }
+
+//        if(requestCode == Activity.RESULT_OK) {
+//            if (requestCode == GALLERY) {
+//                var imageData: Uri? = data?.data
+//                Toast.makeText(requireContext(), imageData.toString(), Toast.LENGTH_SHORT).show()
+//                try {
+//                    val bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, imageData)
+//                    binding.imgCameraPic.setImageBitmap(bitmap)
+//                } catch (e: Exception) {
+//                    e.printStackTrace()
+//                }
+//            }
+//            else if (requestCode == REQUEST_IMAGE_CAPTURE) {
+//                val imageBitmap: Bitmap? = data?.extras?.get("data") as Bitmap
+//                binding.imgCameraPic.setImageBitmap(imageBitmap)
+//            }
+//        }
     }
 }
 
