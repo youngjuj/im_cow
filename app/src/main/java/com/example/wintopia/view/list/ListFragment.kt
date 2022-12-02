25fa5bfc621ce05afc1e60a521294399ca1d8a2f
yougju <alti100@likelion.org>
2022-12-02 10:09:26 +0900

    swipe edit

--- app/src/main/java/com/example/wintopia/view/list/ListFragment.kt
+++ app/src/main/java/com/example/wintopia/view/list/ListFragment.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout.Behavior.setTag
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -26,6 +27,7 @@ import com.example.wintopia.view.utilssd.Constants.TAG
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Float.min
 
 class ListFragment : Fragment() {
 
@@ -65,13 +67,17 @@ class ListFragment : Fragment() {
             private var currentScrollXWhenInActive = 0
             private var initXWhenInActive = 0f
             private var firstInActive = false
+            private var currentPosition: Int? = null
+            private var previousPosition: Int? = null
+            private var currentDx = 0f
+            private var clamp = 0f
 
             override fun getMovementFlags(
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder
             ): Int {
                 val dragFlags = 0
-                val swipeFlags = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                val swipeFlags = ItemTouchHelper.LEFT
                 return makeMovementFlags(dragFlags, swipeFlags)
             }
 
@@ -80,14 +86,16 @@ class ListFragment : Fragment() {
                 viewHolder: RecyclerView.ViewHolder,
                 target: RecyclerView.ViewHolder
             ): Boolean {
-                return true
+                return false
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
             }
 
             override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder): Float {
-                return Integer.MAX_VALUE.toFloat()
+                val isClamped = getTag(viewHolder)
+                setTag(viewHolder, !isClamped && currentDx <= -clamp)
+                return 2f
             }
 
             override fun getSwipeVelocityThreshold(defaultValue: Float): Float {
@@ -104,10 +112,30 @@ class ListFragment : Fragment() {
                 isCurrentlyActive: Boolean
             ) {
                 if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
-                    if (dX == 0f) {
-                        currentScrollX = viewHolder.itemView.scrollX
-                        firstInActive = true
+                    val view = getView(viewHolder)
+
+                    val isClamped = getTag(viewHolder)
+                    val x = clampViewPositionHorizontal(view, dX, isClamped,isCurrentlyActive)
+
+                    if(x == -clamp) {
+                        getView(viewHolder).animate().translationX(-clamp).setDuration(100L).start()
+                        return
                     }
+
+                    currentDx = x
+                    getDefaultUIUtil().onDraw(
+                        c,
+                        recyclerView,
+                        view,
+                        dX,
+                        dY,
+                        actionState,
+                        isCurrentlyActive
+                    )
+//                    if (dX == 0f) {
+//                        currentScrollX = viewHolder.itemView.scrollX
+//                        firstInActive = true
+//                    }
                     if (isCurrentlyActive) {
                         // 스와이프하기
                         var scrollOffset = currentScrollX + (-dX).toInt()
@@ -117,17 +145,58 @@ class ListFragment : Fragment() {
                             scrollOffset = 0
                         }
                         viewHolder.itemView.scrollTo(scrollOffset, 0)
-                    } else {
-                         if (firstInActive) {
-                             firstInActive = false
-                             currentScrollXWhenInActive = viewHolder.itemView.scrollX
-                             initXWhenInActive = dX
-                         }
+//                    } else {
+//                         if (firstInActive) {
+//                             firstInActive = false
+//                             currentScrollXWhenInActive = viewHolder.itemView.scrollX
+//                             initXWhenInActive = dX
+//                         }
 
-                        if (viewHolder.itemView.scrollX < limitScrollX) {
-                            viewHolder.itemView.scrollTo((currentScrollXWhenInActive * dX / initXWhenInActive).toInt(), 0)
-                        }
+//                        if (viewHolder.itemView.scrollX < limitScrollX) {
+//                            viewHolder.itemView.scrollTo((currentScrollXWhenInActive * dX / initXWhenInActive).toInt(), 0)
+//                        }
                     }
+                }
+            }
+
+            private fun clampViewPositionHorizontal(
+                view: View,
+                dX: Float,
+                isClamped: Boolean,
+                isCurrentlyActive: Boolean
+            ): Float {
+                val max: Float = 0f
+
+                val x = if(isClamped) {
+                    if (isCurrentlyActive) dX - clamp else -clamp
+                } else {
+                    dX / 2
+                }
+                return min(x, max)
+            }
+
+            private fun setTag(viewHolder: RecyclerView.ViewHolder, isClamped: Boolean) {
+                viewHolder.itemView.tag = isClamped
+            }
+            private fun getTag(viewHolder: RecyclerView.ViewHolder): Boolean {
+                return viewHolder.itemView.tag as? Boolean ?: false
+            }
+
+            fun setClamp(clamp: Float) {
+                this.clamp = clamp
+            }
+
+            private fun getView(viewHolder: RecyclerView.ViewHolder): View {
+                return (viewHolder as ListVOAdapter.ListVOViewHolder).itemView.rootView
+            }
+
+            fun removePreviousClamp(recyclerView: RecyclerView) {
+                if(currentPosition == previousPosition)
+                    return
+                previousPosition?.let {
+                    val viewHolder = recyclerView.findViewHolderForAdapterPosition(it) ?: return
+                    getView(viewHolder).translationX = 0f
+                    setTag(viewHolder, false)
                 }
             }
 
@@ -135,12 +204,24 @@ class ListFragment : Fragment() {
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder
             ) {
-                super.clearView(recyclerView, viewHolder)
 
-                if (viewHolder.itemView.scrollX > limitScrollX) {
-                    viewHolder.itemView.scrollTo(limitScrollX, 0)
-                } else if (viewHolder.itemView.scrollX < 0) {
-                    viewHolder.itemView.scrollTo(0, 0)
+                currentDx = 0f
+                getDefaultUIUtil().clearView(view)
+                previousPosition = viewHolder.adapterPosition
+
+//                super.clearView(recyclerView, viewHolder)
+
+//                if (viewHolder.itemView.scrollX > limitScrollX) {
+//                    viewHolder.itemView.scrollTo(limitScrollX, 0)
+//                } else if (viewHolder.itemView.scrollX < 0) {
+//                    viewHolder.itemView.scrollTo(0, 0)
+//                }
+            }
+
+            override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+                viewHolder?.let {
+                    currentPosition = viewHolder.adapterPosition
+                    getDefaultUIUtil().onSelected(view)
                 }
             }
         }).apply {
